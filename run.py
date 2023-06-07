d8e882816154168f2cca49bbc987f1f14af89966
Stayin_blick <azeezbello75@gmail.com>
2023-06-07 17:49:59 +0000

    fixed error handling for repeated guess

--- run.py
+++ run.py
@@ -100,10 +100,12 @@ class Game:
             try:
                 while True:
                     user_guess_row, user_guess_column = self.get_guess()
+                while True:
+                    user_guess_row, user_guess_column = self.get_guess()
 
                     if self.computer_board.grid[user_guess_row][user_guess_column] == "M":
                         print("You guessed that one already. Try again.")
-                         print("-----------------------------")
+                        print("-----------------------------")
                     else:
                         break
 
